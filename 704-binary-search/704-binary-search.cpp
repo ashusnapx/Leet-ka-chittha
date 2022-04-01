3025c93160d6a420ca77c0a58a2f167c11f594ee
Ashutosh Kumar <75922918+ashusnapx@users.noreply.github.com>
2022-04-01 10:17:44 +0530

    Time: 76 ms (10.11%), Space: 27.7 MB (12.70%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,26 +1,27 @@
-class Solution {
-public:
-    int search(vector<int>& nums, int target) 
-    {
-        int start = 0;
-        int end = nums.size()-1;
-      
-        while(start<=end)
+class Solution
+{
+    public:
+        int search(vector<int> &nums, int target)
         {
-            int mid = (start+end)/2;
-            if(nums[mid]==target)
+            int start = 0;
+            int end = nums.size() - 1;
+
+            while (start <= end)
             {
-                return mid;
+                int mid = (start + end) / 2;
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                else if (target > nums[mid])
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
             }
-            else if(target > nums[mid])
-            {
-                start = mid + 1;
-            }
-            else
-            {
-                end = mid - 1;
-            }
+            return -1;
         }
-        return -1;
-    }
 };
