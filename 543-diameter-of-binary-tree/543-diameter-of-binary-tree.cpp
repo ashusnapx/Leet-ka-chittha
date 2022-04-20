a73982ec027e1f21f7f9b9e012ef0e5edcecc281
Ashutosh Kumar <75922918+ashusnapx@users.noreply.github.com>
2022-04-20 07:00:27 +0530

    Time: 18 ms (37.90%), Space: 20.2 MB (65.46%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -1,35 +1,37 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    int diameterOfBinaryTree(TreeNode* root) {
-        int diameter = 0;
-        height(root,diameter);
-        return diameter;
-    }
-    
-    private:
-    int height(TreeNode *root,int &diameter)
-    {
-        if(!root)
+class Solution
+{
+    public:
+        int diameterOfBinaryTree(TreeNode *root)
         {
-            return 0;
+            int diameter = 0;
+            height(root, diameter);
+            return diameter;
         }
-        
-        int lh = height(root->left,diameter);
-        int rh = height(root->right,diameter);
-        
-        diameter = max(diameter,lh+rh);
-        
-        return 1+max(lh,rh);
-    }
+
+    private:
+        int height(TreeNode *root, int &diameter)
+        {
+            if (!root)
+            {
+                return 0;
+            }
+
+            int lh = height(root->left, diameter);
+            int rh = height(root->right, diameter);
+
+            diameter = max(diameter, lh + rh);
+
+            return 1 + max(lh, rh);
+        }
 };
