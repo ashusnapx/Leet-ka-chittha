c126fbce51fff9cd5bb30303fdbd4fba0918499e
Ashutosh Kumar <75922918+ashusnapx@users.noreply.github.com>
2022-04-23 01:14:36 +0530

    Time: 36 ms (12.10%), Space: 5.9 MB (76.01%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    #include <cstring>
+    bool isPalindrome(int x) {
+        string a = to_string(x);
+        int i = 0;
+        int j = a.length()-1;
+        while(i<=j)
+        {
+            if(a[i]!=a[j])
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
